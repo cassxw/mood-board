3997b8a601ecd802a3cb73de28b0e128cc3528b9
Cassandra Wallace <cassandra.wallace@flowgear.net>
2025-12-31 16:49:57 +0200

    First pass at styling

--- index.jsx
+++ index.jsx
@@ -5,7 +5,7 @@ export function MoodBoardItem({ color, image, description }) {
 
   return (
     <div className="mood-board-item" style={defaultStyle}>
-      <img className="mood-board-image" src={image} />
+      <img className="mood-board-image" src={image} alt={description} />
       <h3 className="mood-board-text">{description}</h3>
     </div>
   )
@@ -18,21 +18,39 @@ export function MoodBoard() {
 
       <div className="mood-board">
         <MoodBoardItem
-          color="#989829"
+          color="#d3493a"
           image="https://cdn.freecodecamp.org/curriculum/labs/pathway.jpg"
-          description="Pathway"
+          description="Japan - Kyoto Lantern Walk"
         />
 
         <MoodBoardItem
-          color="#7e6967"
+          color="#3b6fa5"
           image="https://cdn.freecodecamp.org/curriculum/labs/santorini.jpg"
-          description="Santorini"
+          description="Greece - Aegean Calm"
         />
 
         <MoodBoardItem
-          color="#055bbe"
+          color="#f19537"
+          image="https://cdn.freecodecamp.org/curriculum/labs/grass.jpg"
+          description="India - Monsoon Meadow"
+        />
+
+        <MoodBoardItem
+          color="#1b7a63"
           image="https://cdn.freecodecamp.org/curriculum/labs/shore.jpg"
-          description="Shore"
+          description="Mexico - Pacific Sunset"
+        />
+
+        <MoodBoardItem
+          color="#f1b932"
+          image="https://cdn.freecodecamp.org/curriculum/labs/ship.jpg"
+          description="Brazil - Coastal Carnival"
+        />
+
+        <MoodBoardItem
+          color="#7ab7d6"
+          image="https://cdn.freecodecamp.org/curriculum/labs/pigeon.jpg"
+          description="Antarctica - Penguin Horizon"
         />
       </div>
     </div>
